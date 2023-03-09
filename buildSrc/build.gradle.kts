5ccb0a5f75f94f5c7501315e168dbae227cc8223
Shreck Ye <ShreckYe@gmail.com>
2023-03-10 07:30:28 +0800

    Bump Compose to 1.3.1 and Kotlin to 1.8.10

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,11 +7,11 @@ repositories {
     //maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
-val kotlinVersion = "1.8.0"
+val kotlinVersion = "1.8.10"
 
 dependencies {
     implementation(kotlin("gradle-plugin", kotlinVersion))
-    implementation("org.jetbrains.compose:compose-gradle-plugin:1.3.0")
+    implementation("org.jetbrains.compose:compose-gradle-plugin:1.3.1")
     implementation("com.huanshankeji:kotlin-common-gradle-plugins:0.3.2")
     implementation("com.huanshankeji.team:gradle-plugins:0.3.2")
 }
