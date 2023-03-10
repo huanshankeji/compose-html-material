5711e3fe1b7069320e477c6a2dc182394ed4d82d
Shreck Ye <ShreckYe@gmail.com>
2023-03-10 20:30:44 +0800

    Don't publish the Javadoc jar in the JS target publication with Huanshankeji Gradle Plugins 0.4.0
    A corresponding commit: https://github.com/huanshankeji/gradle-common/commit/d0c40d46b1e18be0655156a225bab61b6ddedfb4

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,18 +2,19 @@ plugins {
     `kotlin-dsl`
 }
 repositories {
-    //mavenLocal()
+    mavenLocal()
     gradlePluginPortal()
     //maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
 val kotlinVersion = "1.8.10"
+val huanshankejiGradlePluginsVersion = "0.4.0"
 
 dependencies {
     implementation(kotlin("gradle-plugin", kotlinVersion))
     implementation("org.jetbrains.compose:compose-gradle-plugin:1.3.1")
-    implementation("com.huanshankeji:kotlin-common-gradle-plugins:0.3.2")
-    implementation("com.huanshankeji.team:gradle-plugins:0.3.2")
+    implementation("com.huanshankeji:kotlin-common-gradle-plugins:$huanshankejiGradlePluginsVersion")
+    implementation("com.huanshankeji.team:gradle-plugins:$huanshankejiGradlePluginsVersion")
 }
 
 configurations.all {
