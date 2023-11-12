594e59272f6966bc3bf20fc1095eee02a64fe426
Shreck Ye <ShreckYe@gmail.com>
2023-11-13 03:30:38 +0800

    Resolve an error caused strangely by bumping the Gradle wrapper
    The error:
    ```text
    Execution failed for task ':compose-web-material-gradle-plugins:compileKotlin'.
    > Could not resolve all files for configuration ':compose-web-material-gradle-plugins:kotlinCompilerPluginClasspathMain'.
       > Could not find org.jetbrains.kotlin:kotlin-sam-with-receiver-compiler-plugin-embeddable:1.8.20.
         Searched in the following locations:
           - https://repo.maven.apache.org/maven2/org/jetbrains/kotlin/kotlin-sam-with-receiver-compiler-plugin-embeddable/1.8.20/kotlin-sam-with-receiver-compiler-plugin-embeddable-1.8.20.pom
         Required by:
             project :compose-web-material-gradle-plugins
    ```

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,12 +7,12 @@ repositories {
     //maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
-val kotlinVersion = "1.8.20"
+val kotlinVersion = "1.9.20"
 val huanshankejiGradlePluginsVersion = "0.4.1"
 
 dependencies {
     implementation(kotlin("gradle-plugin", kotlinVersion))
-    implementation("org.jetbrains.compose:compose-gradle-plugin:1.4.0")
+    implementation("org.jetbrains.compose:compose-gradle-plugin:1.5.10")
     implementation("com.huanshankeji:kotlin-common-gradle-plugins:$huanshankejiGradlePluginsVersion")
     implementation("com.huanshankeji.team:gradle-plugins:$huanshankejiGradlePluginsVersion")
 }
