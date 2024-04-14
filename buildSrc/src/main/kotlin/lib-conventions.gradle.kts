3a00375c6a1c43f18dbd12461446ae4b8124c955
Shreck Ye <ShreckYe@gmail.com>
2024-04-15 03:14:14 +0800

    Remove some unnecessary imports

--- buildSrc/src/main/kotlin/lib-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/lib-conventions.gradle.kts
@@ -1,9 +1,3 @@
-import com.huanshankeji.team.`Shreck Ye`
-import com.huanshankeji.team.pomForTeamDefaultOpenSource
-import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.api.tasks.bundling.Jar
-import org.gradle.kotlin.dsl.*
-
 plugins {
     id("com.huanshankeji.kotlin-multiplatform-js-browser-conventions")
     id("org.jetbrains.compose")
