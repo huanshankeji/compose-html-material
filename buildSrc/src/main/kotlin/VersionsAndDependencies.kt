210aa21a70f36856f06447a6a5af74f07d06d6b3
Shreck Ye <ShreckYe@gmail.com>
2024-04-24 16:12:20 +0800

    Bump dependency versions and run `kotlinUpgradeYarnLock`

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -3,8 +3,8 @@ import com.huanshankeji.CommonDependencies
 const val projectVersion = "0.3.0-SNAPSHOT"
 
 object DependencyVersions {
-    val kobweb = "0.17.1"
-    val materialWeb = "1.4.0"
+    val kobweb = "0.17.3"
+    val materialWeb = "1.4.1"
 
     val webcomponents = "2.6.0"
     val mwc = "0.25.3"
