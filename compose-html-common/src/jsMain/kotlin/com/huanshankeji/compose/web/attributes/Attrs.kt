30a6b8593d54de667baad5bf943847ea558581b7
Shreck Ye <ShreckYe@gmail.com>
2024-04-29 04:04:35 +0800

    Introduce the new `attrIfNotNull` extension functions and adapt the existing attribute extensions to use them, and rename all the attribute value parameters to `value` for consistency

--- compose-html-common/src/jsMain/kotlin/com/huanshankeji/compose/web/attributes/Attrs.kt
+++ compose-html-common/src/jsMain/kotlin/com/huanshankeji/compose/web/attributes/Attrs.kt
@@ -1,21 +1,32 @@
 package com.huanshankeji.compose.web.attributes
 
-import com.varabyte.kobweb.compose.css.disabled
 import org.jetbrains.compose.web.attributes.AttrsScope
-import org.jetbrains.compose.web.attributes.AutoComplete
-import org.jetbrains.compose.web.attributes.InputType
 
 /**
  * @see AttrsScope.attr
  */
-fun AttrsScope<*>.attr(attr: String, isPresent: Boolean = true) =
-    attr(attr, isPresent.toString())
+fun AttrsScope<*>.attr(attr: String, value: Boolean = true) =
+    attr(attr, value.toString())
 
 /**
  * [Int] attributes are used in Compose HTML. See [org.jetbrains.compose.web.attributes.maxLength] for example.
  */
-fun AttrsScope<*>.attr(attr: String, number: Int) =
-    attr(attr, number.toString())
+fun AttrsScope<*>.attr(attr: String, value: Int) =
+    attr(attr, value.toString())
+
+
+fun AttrsScope<*>.attrIfNotNull(attr: String, value: String?) {
+    value?.let { attr(attr, it) }
+}
+
+fun AttrsScope<*>.attrIfNotNull(attr: String, isPresent: Boolean?) {
+    isPresent?.let { attr(attr, it) }
+}
+
+fun AttrsScope<*>.attrIfNotNull(attr: String, value: Int?) {
+    value?.let { attr(attr, it) }
+}
+
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/slot
 fun AttrsScope<*>.slot(value: String) =
--- compose-html-common/src/jsMain/kotlin/com/huanshankeji/compose/web/attributes/ext/Attrs.kt
+++ compose-html-common/src/jsMain/kotlin/com/huanshankeji/compose/web/attributes/ext/Attrs.kt
@@ -1,15 +1,17 @@
 package com.huanshankeji.compose.web.attributes.ext
 
-import com.huanshankeji.compose.web.attributes.attr
-import com.varabyte.kobweb.compose.css.disabled
+import com.huanshankeji.compose.web.attributes.attrIfNotNull
 import org.jetbrains.compose.web.attributes.AttrsScope
 import org.jetbrains.compose.web.attributes.AutoComplete
 import org.jetbrains.compose.web.attributes.InputType
 
 /*
-Attributes in this package are for `HTMLElement`s which don't implement the interfaces (such as `HTMLInputElement`) of the elements they behave like,
+Attributes in the `ext` package are one of 2 kinds:
+1. Those for `HTMLElement`s which don't implement the interfaces (such as `HTMLInputElement`) of the elements they behave like,
 such as the `HTMLElement`s of those components in Material Web.
 These attributes should not be universally available on most elements.
+1. Those taking nullable values.
+
 Also consider moving to a `compose-html-material-common` module and depend on them with `implementation`.
  */
 
@@ -28,94 +30,82 @@ fun AttrsScope<*>.disabled(disabled: Boolean?) {
 
 // https://www.w3schools.com/tags/att_href.asp
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#href
-fun AttrsScope<*>.href(href: String?) {
-    href?.let { attr("href", it) }
-}
+fun AttrsScope<*>.href(value: String?) =
+    attrIfNotNull("href", value)
 
 // https://www.w3schools.com/tags/att_target.asp
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#target
-fun AttrsScope<*>.target(target: String?) {
-    target?.let { attr("target", it) }
-}
+fun AttrsScope<*>.target(value: String?) =
+    attrIfNotNull("target", value)
+
 
 // https://www.w3schools.com/tags/att_input_type.asp
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#type
-fun AttrsScope<*>.type(type: String?) {
-    type?.let { attr("type", it) }
-}
+fun AttrsScope<*>.type(value: String?) =
+    attrIfNotNull("type", value)
 
 // https://www.w3schools.com/tags/att_value.asp
-fun AttrsScope<*>.value(value: String?) {
-    value?.let { attr("value", it) }
-}
+fun AttrsScope<*>.value(value: String?) =
+    attrIfNotNull("value", value)
+
 
 // https://www.w3schools.com/tags/att_name.asp
-fun AttrsScope<*>.name(name: String?) {
-    name?.let { attr("name", it) }
-}
+fun AttrsScope<*>.name(value: String?) =
+    attrIfNotNull("name", value)
+
 
 // https://www.w3schools.com/tags/att_form.asp
-fun AttrsScope<*>.form(formId: String?) {
-    formId?.let { attr("form", it) }
-}
+fun AttrsScope<*>.form(value: String?) =
+    attrIfNotNull("form", value)
 
 // https://www.w3schools.com/tags/att_required.asp
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/required
-fun AttrsScope<*>.required(required: Boolean?) {
-    required?.let { attr("required", it) }
-}
+fun AttrsScope<*>.required(value: Boolean?) =
+    attrIfNotNull("required", value)
+
 // https://www.geeksforgeeks.org/html-label-attribute/
-fun AttrsScope<*>.label(label: String?) {
-    label?.let { attr("label", it) }
-}
+fun AttrsScope<*>.label(value: String?) =
+    attrIfNotNull("label", value)
 
 // copied and adapted from `Attrs.kt` in `org.jetbrains.compose.web.attributes`
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#max
-fun AttrsScope<*>.max(value: String?) {
-    value?.let { attr("max", it) }
-}
+fun AttrsScope<*>.max(value: String?) =
+    attrIfNotNull("max", value)
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#maxlength
-fun AttrsScope<*>.maxLength(value: Int?) {
-    value?.let { attr("maxlength", it) }
-}
+fun AttrsScope<*>.maxLength(value: Int?) =
+    attrIfNotNull("maxlength", value)
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#min
-fun AttrsScope<*>.min(value: String?) {
-    value?.let { attr("min", it) }
-}
+fun AttrsScope<*>.min(value: String?) =
+    attrIfNotNull("min", value)
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#minlength
-fun AttrsScope<*>.minLength(value: Int?) {
-    value?.let { attr("minlength", it) }
-}
+fun AttrsScope<*>.minLength(value: Int?) =
+    attrIfNotNull("minlength", value)
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#pattern
-fun AttrsScope<*>.pattern(value: String?) {
-    value?.let { attr("pattern", it) }
-}
+fun AttrsScope<*>.pattern(value: String?) =
+    attrIfNotNull("pattern", value)
 
 // https://www.w3schools.com/tags/att_input_placeholder.asp
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/placeholder
-fun AttrsScope<*>.placeholder(value: String?) {
-    value?.let { attr("placeholder", it) }
-}
+fun AttrsScope<*>.placeholder(value: String?) =
+    attrIfNotNull("placeholder", value)
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#readonly
-fun AttrsScope<*>.readOnly(value: Boolean?) {
-    value?.let { attr("readonly", it) }
-}
+fun AttrsScope<*>.readOnly(value: Boolean?) =
+    attrIfNotNull("readonly", value)
+
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#multiple
-fun AttrsScope<*>.multiple(value: Boolean?) {
-    value?.let { attr("multiple", it) }
-}
+fun AttrsScope<*>.multiple(value: Boolean?) =
+    attrIfNotNull("multiple", value)
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#step
-fun AttrsScope<*>.step(value: String?) {
-    value?.let { attr("step", it) }
-}
+fun AttrsScope<*>.step(value: String?) =
+    attrIfNotNull("step", value)
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#input_types
 fun AttrsScope<*>.type(value: InputType<*>?) {
@@ -124,5 +114,5 @@ fun AttrsScope<*>.type(value: InputType<*>?) {
 
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete
 fun AttrsScope<*>.autoComplete(value: AutoComplete?) {
-    value?.let { attr("autocomplete", value.unsafeCast<String>()) }
+    value?.let { attr("autocomplete", it.unsafeCast<String>()) }
 }
