huanshankeji/compose-html-material

commit 1923360f11f5a71ba15e8fcede461607ced18e97

Move some attributes and extensions to the `ext` packages, see the first comment in `Attrs.kt` in `com.huanshankeji.compose.web.attributes.ext` for more details

compose-html-common/src/jsMain/kotlin/com/huanshankeji/compose/web/attributes/Attrs.kt
@@ -17,120 +17,6 @@ fun AttrsScope<*>.attr(attr: String, isPresent: Boolean = true) =
 fun AttrsScope<*>.attr(attr: String, number: Int) =
     attr(attr, number.toString())
 
-// https://www.w3schools.com/accessibility/accessibility_labels.php
-// https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-label
-fun AttrsScope<*>.ariaLabel(value: String) =
-    attr("aria-label", value)
-
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/slot
 fun AttrsScope<*>.slot(value: String) =
     attr("slot", value)
-
-// Consider moving what's below to a `InputAttrs.kt` file in a `compose-html-material-common` module and depend on them with `implementation`.
-
-// https://www.w3schools.com/tags/att_disabled.asp
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/disabled
-fun AttrsScope<*>.disabled(disabled: Boolean?) {
-    disabled?.let { disabled(it) }
-}
-
-// https://www.w3schools.com/tags/att_href.asp
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#href
-fun AttrsScope<*>.href(href: String?) {
-    href?.let { attr("href", it) }
-}
-
-// https://www.w3schools.com/tags/att_target.asp
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#target
-fun AttrsScope<*>.target(target: String?) {
-    target?.let { attr("target", it) }
-}
-
-// https://www.w3schools.com/tags/att_input_type.asp
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#type
-fun AttrsScope<*>.type(type: String?) {
-    type?.let { attr("type", it) }
-}
-
-// https://www.w3schools.com/tags/att_value.asp
-fun AttrsScope<*>.value(value: String?) {
-    value?.let { attr("value", it) }
-}
-
-// https://www.w3schools.com/tags/att_name.asp
-fun AttrsScope<*>.name(name: String?) {
-    name?.let { attr("name", it) }
-}
-
-// https://www.w3schools.com/tags/att_form.asp
-fun AttrsScope<*>.form(formId: String?) {
-    formId?.let { attr("form", it) }
-}
-
-// https://www.w3schools.com/tags/att_required.asp
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/required
-fun AttrsScope<*>.required(required: Boolean?) {
-    required?.let { attr("required", it) }
-}
-
-fun AttrsScope<*>.label(label: String?) {
-    label?.let { attr("label", it) }
-}
-
-// copied and adapted from `Attrs.kt` in `org.jetbrains.compose.web.attributes`
-
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#max
-fun AttrsScope<*>.max(value: String?) {
-    value?.let { attr("max", it) }
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#maxlength
-fun AttrsScope<*>.maxLength(value: Int?) {
-    value?.let { attr("maxlength", it) }
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#min
-fun AttrsScope<*>.min(value: String?) {
-    value?.let { attr("min", it) }
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#minlength
-fun AttrsScope<*>.minLength(value: Int?) {
-    value?.let { attr("minlength", it) }
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#pattern
-fun AttrsScope<*>.pattern(value: String?) {
-    value?.let { attr("pattern", it) }
-}
-
-// https://www.w3schools.com/tags/att_input_placeholder.asp
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/placeholder
-fun AttrsScope<*>.placeholder(value: String?) {
-    value?.let { attr("placeholder", it) }
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#readonly
-fun AttrsScope<*>.readOnly(value: Boolean?) {
-    value?.let { attr("readonly", it) }
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#multiple
-fun AttrsScope<*>.multiple(value: Boolean?) {
-    value?.let { attr("multiple", it) }
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#step
-fun AttrsScope<*>.step(value: String?) {
-    value?.let { attr("step", it) }
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#input_types
-fun AttrsScope<*>.type(value: InputType<*>?) {
-    value?.let { attr("type", it.typeStr) }
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete
-fun AttrsScope<*>.autoComplete(value: AutoComplete?) {
-    value?.let { attr("autocomplete", value.unsafeCast<String>()) }
-}

compose-html-common/src/jsMain/kotlin/com/huanshankeji/compose/web/attributes/ext/Attrs.kt
@@ -1,0 +1,128 @@
+package com.huanshankeji.compose.web.attributes.ext
+
+import com.huanshankeji.compose.web.attributes.attr
+import com.varabyte.kobweb.compose.css.disabled
+import org.jetbrains.compose.web.attributes.AttrsScope
+import org.jetbrains.compose.web.attributes.AutoComplete
+import org.jetbrains.compose.web.attributes.InputType
+
+/*
+Attributes in this package are for `HTMLElement`s which don't implement the interfaces (such as `HTMLInputElement`) of the elements they behave like,
+such as the `HTMLElement`s of those components in Material Web.
+These attributes should not be universally available on most elements.
+ */
+
+
+// https://www.w3schools.com/accessibility/accessibility_labels.php
+// https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-label
+fun AttrsScope<*>.ariaLabel(value: String) =
+    attr("aria-label", value)
+
+// Consider moving what's below to a `InputAttrs.kt` file in a `compose-html-material-common` module and depend on them with `implementation`.
+
+// https://www.w3schools.com/tags/att_disabled.asp
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/disabled
+fun AttrsScope<*>.disabled(disabled: Boolean?) {
+    disabled?.let { disabled(it) }
+}
+
+// https://www.w3schools.com/tags/att_href.asp
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#href
+fun AttrsScope<*>.href(href: String?) {
+    href?.let { attr("href", it) }
+}
+
+// https://www.w3schools.com/tags/att_target.asp
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#target
+fun AttrsScope<*>.target(target: String?) {
+    target?.let { attr("target", it) }
+}
+
+// https://www.w3schools.com/tags/att_input_type.asp
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#type
+fun AttrsScope<*>.type(type: String?) {
+    type?.let { attr("type", it) }
+}
+
+// https://www.w3schools.com/tags/att_value.asp
+fun AttrsScope<*>.value(value: String?) {
+    value?.let { attr("value", it) }
+}
+
+// https://www.w3schools.com/tags/att_name.asp
+fun AttrsScope<*>.name(name: String?) {
+    name?.let { attr("name", it) }
+}
+
+// https://www.w3schools.com/tags/att_form.asp
+fun AttrsScope<*>.form(formId: String?) {
+    formId?.let { attr("form", it) }
+}
+
+// https://www.w3schools.com/tags/att_required.asp
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/required
+fun AttrsScope<*>.required(required: Boolean?) {
+    required?.let { attr("required", it) }
+}
+// https://www.geeksforgeeks.org/html-label-attribute/
+fun AttrsScope<*>.label(label: String?) {
+    label?.let { attr("label", it) }
+}
+
+// copied and adapted from `Attrs.kt` in `org.jetbrains.compose.web.attributes`
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#max
+fun AttrsScope<*>.max(value: String?) {
+    value?.let { attr("max", it) }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#maxlength
+fun AttrsScope<*>.maxLength(value: Int?) {
+    value?.let { attr("maxlength", it) }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#min
+fun AttrsScope<*>.min(value: String?) {
+    value?.let { attr("min", it) }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#minlength
+fun AttrsScope<*>.minLength(value: Int?) {
+    value?.let { attr("minlength", it) }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#pattern
+fun AttrsScope<*>.pattern(value: String?) {
+    value?.let { attr("pattern", it) }
+}
+
+// https://www.w3schools.com/tags/att_input_placeholder.asp
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/placeholder
+fun AttrsScope<*>.placeholder(value: String?) {
+    value?.let { attr("placeholder", it) }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#readonly
+fun AttrsScope<*>.readOnly(value: Boolean?) {
+    value?.let { attr("readonly", it) }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#multiple
+fun AttrsScope<*>.multiple(value: Boolean?) {
+    value?.let { attr("multiple", it) }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#step
+fun AttrsScope<*>.step(value: String?) {
+    value?.let { attr("step", it) }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#input_types
+fun AttrsScope<*>.type(value: InputType<*>?) {
+    value?.let { attr("type", it.typeStr) }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete
+fun AttrsScope<*>.autoComplete(value: AutoComplete?) {
+    value?.let { attr("autocomplete", value.unsafeCast<String>()) }
+}

compose-html-common/src/jsMain/kotlin/com/huanshankeji/compose/web/attributes/ext/EventAttrs.kt
@@ -1,4 +1,4 @@
-package com.huanshankeji.compose.web.attributes
+package com.huanshankeji.compose.web.attributes.ext
 
 import androidx.compose.web.events.SyntheticEvent
 import org.jetbrains.compose.web.attributes.AttrsScope

compose-html-common/src/jsMain/kotlin/com/huanshankeji/compose/web/dom/ext/dom.kt
@@ -1,4 +1,4 @@
-package com.huanshankeji.compose.web.dom
+package com.huanshankeji.compose.web.dom.ext
 
 import org.w3c.dom.HTMLElement
 

compose-html-material3/src/jsMain/kotlin/com/huanshankeji/compose/html/material3/MdButton.kt
@@ -1,7 +1,8 @@
 package com.huanshankeji.compose.html.material3
 
 import androidx.compose.runtime.Composable
-import com.huanshankeji.compose.web.attributes.*
+import com.huanshankeji.compose.web.attributes.Attrs
+import com.huanshankeji.compose.web.attributes.ext.*
 import org.jetbrains.compose.web.attributes.AttrsScope
 import org.jetbrains.compose.web.dom.ElementScope
 import org.jetbrains.compose.web.dom.TagElement

compose-html-material3/src/jsMain/kotlin/com/huanshankeji/compose/html/material3/MdCheckbox.kt
@@ -1,7 +1,9 @@
 package com.huanshankeji.compose.html.material3
 
 import androidx.compose.runtime.Composable
-import com.huanshankeji.compose.web.attributes.*
+import com.huanshankeji.compose.web.attributes.Attrs
+import com.huanshankeji.compose.web.attributes.attr
+import com.huanshankeji.compose.web.attributes.ext.*
 import org.jetbrains.compose.web.dom.TagElement
 import org.w3c.dom.HTMLElement
 

compose-html-material3/src/jsMain/kotlin/com/huanshankeji/compose/html/material3/MdSwitch.kt
@@ -1,7 +1,9 @@
 package com.huanshankeji.compose.html.material3
 
 import androidx.compose.runtime.Composable
-import com.huanshankeji.compose.web.attributes.*
+import com.huanshankeji.compose.web.attributes.Attrs
+import com.huanshankeji.compose.web.attributes.attr
+import com.huanshankeji.compose.web.attributes.ext.*
 import org.jetbrains.compose.web.dom.Label
 import org.jetbrains.compose.web.dom.TagElement
 import org.jetbrains.compose.web.dom.Text

compose-html-material3/src/jsMain/kotlin/com/huanshankeji/compose/html/material3/MdTextField.kt
@@ -1,7 +1,10 @@
 package com.huanshankeji.compose.html.material3
 
 import androidx.compose.runtime.Composable
-import com.huanshankeji.compose.web.attributes.*
+import com.huanshankeji.compose.web.attributes.Attrs
+import com.huanshankeji.compose.web.attributes.attr
+import com.huanshankeji.compose.web.attributes.ext.*
+import com.huanshankeji.compose.web.attributes.slot
 import org.jetbrains.compose.web.attributes.AttrsScope
 import org.jetbrains.compose.web.attributes.AutoComplete
 import org.jetbrains.compose.web.attributes.InputMode
@@ -22,7 +25,7 @@ private fun CommonTextField(
     disabled: Boolean?,
     error: Boolean?,
     errorText: String?,
-    label: String?,
+    label: String?, // This attribute seems to have different semantics here from its original semantics in HTML.
     required: Boolean?,
     value: String?,
     prefixText: String?,

commit 8278613922a17a5b6a6199121b768604710e3785

Add some new attributes for some new functionalities in "compose-multiplatform-material"
Fix some broken references caused by commit 1923360f11f5a71ba15e8fcede461607ced18e97 BTW.

compose-html-common/src/jsMain/kotlin/com/huanshankeji/compose/web/attributes/AttrConversion.kt
@@ -2,8 +2,11 @@ package com.huanshankeji.compose.web.attributes
 
 // consider moving to a "web-common" or "html-common" module
 
-fun Boolean.isTrueOrNull() : Boolean? =
+fun Boolean.isTrueOrNull(): Boolean? =
     if (this) true else null
 
 fun Boolean.isFalseOrNull(): Boolean? =
     if (this) null else false
+
+fun Boolean.toOnOrOff(): String =
+    if (this) "on" else "off"

compose-html-common/src/jsMain/kotlin/com/huanshankeji/compose/web/attributes/Attrs.kt
@@ -3,6 +3,7 @@ package com.huanshankeji.compose.web.attributes
 import org.jetbrains.compose.web.attributes.AttrsScope
 
 /**
+ * Adds an attribute that is made present by its key aka [attr].
  * @see AttrsScope.attr
  */
 fun AttrsScope<*>.attr(attr: String, value: Boolean = true) =
@@ -12,6 +13,12 @@ fun AttrsScope<*>.attr(attr: String, value: Boolean = true) =
  * [Int] attributes are used in Compose HTML. See [org.jetbrains.compose.web.attributes.maxLength] for example.
  */
 fun AttrsScope<*>.attr(attr: String, value: Int) =
+    attr(attr, value.toString())
+
+/**
+ * Adds an attribute that has an explicit [Boolean] value unlike [attr].
+ */
+fun AttrsScope<*>.booleanAttr(attr: String, value: Boolean) =
     attr(attr, value.toString())
 
 
@@ -31,3 +38,45 @@ fun AttrsScope<*>.attrIfNotNull(attr: String, value: Int?) {
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/slot
 fun AttrsScope<*>.slot(value: String) =
     attr("slot", value)
+
+
+enum class AutoCapitalize(val strValue: String, val alternativeStrValue: String? = null) {
+    None("none", "off"), Sentences("sentences", "on"), Words("words"), Characters("characters");
+
+    companion object {
+        val valueSet = entries.asSequence().map { it.strValue }.toSet()
+    }
+}
+
+// This is actually a global attribute
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/autocapitalize
+fun AttrsScope<*>.autoCapitalize(value: String) =
+    attr("autocapitalize", value)
+
+fun AttrsScope<*>.autoCapitalize(value: AutoCapitalize) =
+    autoCapitalize(value.strValue)
+
+
+// Safari only
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Input#attr-autocorrect
+fun AttrsScope<*>.autocorrect(value: String) =
+    attr("autocorrect", value)
+
+fun AttrsScope<*>.autocorrect(onOrOff: Boolean) =
+    attr("autocorrect", onOrOff.toOnOrOff())
+
+
+enum class EnterKeyHint(val strValue: String) {
+    Enter("enter"), Done("done"), Go("go"), Next("next"), Previous("previous"), Search("search"), Send("send");
+
+    companion object {
+        val valueSet = entries.map { it.strValue }.toSet()
+    }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/enterkeyhint
+fun AttrsScope<*>.enterKeyHint(value: String) =
+    attr("enterkeyhint", value)
+
+fun AttrsScope<*>.enterKeyHint(value: EnterKeyHint) =
+    enterKeyHint(value.strValue)

compose-html-common/src/jsMain/kotlin/com/huanshankeji/compose/web/attributes/ext/Attrs.kt
@@ -1,6 +1,10 @@
 package com.huanshankeji.compose.web.attributes.ext
 
+import com.huanshankeji.compose.web.attributes.AutoCapitalize
+import com.huanshankeji.compose.web.attributes.EnterKeyHint
 import com.huanshankeji.compose.web.attributes.attrIfNotNull
+import com.huanshankeji.compose.web.attributes.autoCapitalize
+import com.huanshankeji.compose.web.attributes.enterKeyHint
 import org.jetbrains.compose.web.attributes.AttrsScope
 import org.jetbrains.compose.web.attributes.AutoComplete
 import org.jetbrains.compose.web.attributes.InputType
@@ -116,4 +120,20 @@ fun AttrsScope<*>.type(value: InputType<*>?) {
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete
 fun AttrsScope<*>.autoComplete(value: AutoComplete?) {
     value?.let { attr("autocomplete", it.unsafeCast<String>()) }
+}
+
+
+// This is actually a global attribute
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/autocapitalize
+fun AttrsScope<*>.autoCapitalize(value: String?) =
+    value?.let { this@autoCapitalize.autoCapitalize(value) }
+
+fun AttrsScope<*>.autoCapitalizeRequiringValid(value: String) {
+    require(value in AutoCapitalize.valueSet)
+    autoCapitalize(value)
+}
+
+
+fun AttrsScope<*>.enterKeyHintIfValid(value: String) {
+    if (value in EnterKeyHint.valueSet) enterKeyHint(value)
 }

compose-html-common/src/jsMain/kotlin/com/huanshankeji/compose/web/attributes/ext/EventAttrs.kt
@@ -6,7 +6,7 @@ import org.jetbrains.compose.web.attributes.EventsListenerScope
 import org.w3c.dom.HTMLElement
 
 /**
- * @see com.huanshankeji.compose.web.dom.value
+ * @see com.huanshankeji.compose.web.dom.ext.value
  */
 //fun <Element : EventTarget> EventsListenerScope.onInput(
 fun <TElement : HTMLElement> AttrsScope<TElement>.onInput(

compose-html-common/src/jsMain/kotlin/com/huanshankeji/compose/web/dom/ext/dom.kt
@@ -3,7 +3,7 @@ package com.huanshankeji.compose.web.dom.ext
 import org.w3c.dom.HTMLElement
 
 /**
- * @see com.huanshankeji.compose.web.attributes.onInput
+ * @see com.huanshankeji.compose.web.attributes.ext.onInput
  */
 val HTMLElement.value: String
     get() = asDynamic().value as String