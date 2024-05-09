49057370fb8d9fd43b181bae4455ee277fc97b75
Shreck Ye <ShreckYe@gmail.com>
2024-05-09 09:06:26 +0800

    Add the card composables

---
+++ compose-html-material3/src/jsMain/kotlin/com/huanshankeji/compose/html/material3/MaterialWebLabsApi.kt
@@ -0,0 +1,9 @@
+package com.huanshankeji.compose.html.material3
+
+@RequiresOptIn(
+    "This API depends on components in the Material Web labs directory, which contains experimental features that are not recommended for production. " +
+            "See https://github.com/material-components/material-web/tree/main/labs for more details.",
+    RequiresOptIn.Level.WARNING
+)
+@Retention(AnnotationRetention.BINARY)
+annotation class MaterialWebLabsApi
