73616357e00c34e2f090d6fa55cd331332f1602a
Shreck Ye <ShreckYe@gmail.com>
2024-10-17 14:54:50 +0800

    Adapt to "com.huanshankeji:common-gradle-dependencies:0.7.2-20241016-SNAPSHOT"

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,12 +2,10 @@ plugins {
     `kotlin-dsl`
 }
 repositories {
-    //mavenLocal()
+    mavenLocal() // TODO comment out when not needed
     gradlePluginPortal()
     //maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
-
-val huanshankejiGradlePluginsVersion = "0.5.1"
 
 dependencies {
     // With Kotlin 2.0.20, a "Could not parse POM" build error occurs in the JVM projects of some dependent projects.
@@ -15,7 +13,8 @@ dependencies {
     implementation(kotlin("gradle-plugin", kotlinVersion))
     implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:$kotlinVersion")
     implementation("org.jetbrains.compose:compose-gradle-plugin:1.6.10")
+    val huanshankejiGradlePluginsVersion = "0.6.0-SNAPSHOT" // TODO don't use a snapshot version in a main branch
     implementation("com.huanshankeji:kotlin-common-gradle-plugins:$huanshankejiGradlePluginsVersion")
     implementation("com.huanshankeji.team:gradle-plugins:$huanshankejiGradlePluginsVersion")
-    implementation("com.huanshankeji:common-gradle-dependencies:0.7.1-20240516")
+    implementation("com.huanshankeji:common-gradle-dependencies:0.7.2-20241016-SNAPSHOT") // TODO don't use a snapshot version in a main branch
 }
