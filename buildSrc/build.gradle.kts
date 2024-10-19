0ddd8160c33fd44128322857dd28bfc09529cca0
Shreck Ye <ShreckYe@gmail.com>
2024-10-19 18:05:53 +0800

    Bump our Gradle dependencies to the release versions
    The Gradle plugins are broken on the Gradle Plugin Portal. Temporarily the local ones are loaded.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,8 +13,8 @@ dependencies {
     implementation(kotlin("gradle-plugin", kotlinVersion))
     implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:$kotlinVersion")
     implementation("org.jetbrains.compose:compose-gradle-plugin:1.7.0")
-    val huanshankejiGradlePluginsVersion = "0.6.0-SNAPSHOT" // TODO don't use a snapshot version in a main branch
+    val huanshankejiGradlePluginsVersion = "0.6.0" // don't use a snapshot version in a main branch
     implementation("com.huanshankeji:kotlin-common-gradle-plugins:$huanshankejiGradlePluginsVersion")
     implementation("com.huanshankeji.team:gradle-plugins:$huanshankejiGradlePluginsVersion")
-    implementation("com.huanshankeji:common-gradle-dependencies:0.8.0-20241016-SNAPSHOT") // TODO don't use a snapshot version in a main branch
+    implementation("com.huanshankeji:common-gradle-dependencies:0.8.0-20241016") // don't use a snapshot version in a main branch
 }
