314a8e1198bfcbc4c326645ce531951c9fffc40c
Shreck Ye <ShreckYe@gmail.com>
2024-11-06 10:17:35 +0800

    Resolve the Dokka "could not resolve" issue with the official solution
    A source commit: https://github.com/huanshankeji/gradle-common/commit/c1c69c36fc82be8876c516a3dc06570f1271d857

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,6 @@ plugins {
     id("org.jetbrains.dokka")
 }
 
-// workaround for https://github.com/Kotlin/dokka/issues/3903 from https://github.com/Kotlin/dokka/issues/2260 TODO remove when it's fixed
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     listOf(
         "compose-html-common",
