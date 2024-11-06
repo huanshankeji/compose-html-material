91fedc592cf2a96c1d2ddaf59d5c837e88af1269
Shreck Ye <ShreckYe@gmail.com>
2024-11-06 11:01:35 +0800

    Deploy for the first time

--- buildSrc/src/main/kotlin/dokka-convention.gradle.kts
+++ buildSrc/src/main/kotlin/dokka-convention.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dokka {
     dokkaSourceSets.all {
         sourceLink {
-            remoteUrl("https://github.com/huanshankeji/compose-html-material/tree/v${version}/${project.name}")
+            remoteUrl("https://github.com/huanshankeji/compose-html-material/tree/a1f4168de1bfbab045dbe95daf2d0bd7a59ea46b/${project.name}")
             remoteLineSuffix.set("#L")
         }
     }
