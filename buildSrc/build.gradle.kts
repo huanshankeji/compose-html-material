6becf105d13b0c738b6c62330871d0aad6057e76
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-01 03:30:23 +0800

    Bump Compose Multiplatform to 1.7.1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     val kotlinVersion = "2.0.10"
     implementation(kotlin("gradle-plugin", kotlinVersion))
     implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:$kotlinVersion")
-    implementation("org.jetbrains.compose:compose-gradle-plugin:1.7.0")
+    implementation("org.jetbrains.compose:compose-gradle-plugin:1.7.1")
     val huanshankejiGradlePluginsVersion = "0.6.0" // don't use a snapshot version in a main branch
     implementation("com.huanshankeji:kotlin-common-gradle-plugins:$huanshankejiGradlePluginsVersion")
     implementation("com.huanshankeji.team:gradle-plugins:$huanshankejiGradlePluginsVersion")
