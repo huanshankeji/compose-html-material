37add04f55bc1de0d6b2af4c7398dc1e2477e052
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-05 02:56:27 +0800

    Bump our common-gradle-dependencies to v0.9.0-20241203 and our Gradle plugins to v0.9.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,9 +13,9 @@ dependencies {
     implementation(kotlin("gradle-plugin", kotlinVersion))
     implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:$kotlinVersion")
     implementation("org.jetbrains.compose:compose-gradle-plugin:1.7.1")
-    val huanshankejiGradlePluginsVersion = "0.8.0" // don't use a snapshot version in a main branch
+    val huanshankejiGradlePluginsVersion = "0.9.0" // don't use a snapshot version in a main branch
     implementation("com.huanshankeji:kotlin-common-gradle-plugins:$huanshankejiGradlePluginsVersion")
     implementation("com.huanshankeji.team:gradle-plugins:$huanshankejiGradlePluginsVersion")
-    implementation("com.huanshankeji:common-gradle-dependencies:0.8.0-20241016") // don't use a snapshot version in a main branch
+    implementation("com.huanshankeji:common-gradle-dependencies:0.9.0-20241203") // don't use a snapshot version in a main branch
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:2.0.0-Beta")
 }
