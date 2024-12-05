74b0ff8f16def1d9533c0a9b6a7771ac151abc5a
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-05 15:58:01 +0800

    Remove an outdated comment

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,6 @@ repositories {
 }
 
 dependencies {
-    // With Kotlin 2.0.20, a "Could not parse POM" build error occurs in the JVM projects of some dependent projects.
     val kotlinVersion = "2.1.0"
     implementation(kotlin("gradle-plugin", kotlinVersion))
     implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:$kotlinVersion")
