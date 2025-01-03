514528dbbeb15d8c301fbd9f225fa1b1c16788b3
Yongshun Shreck Ye <ShreckYe@gmail.com>
2025-01-04 04:17:59 +0800

    Simplify the code in "MdFab.kt" into a minimal example to reproduce the Kotlin 2.0 type inference issue
    The issue seems not related to `Composable`.

--- compose-html-material3/src/jsMain/kotlin/com/huanshankeji/compose/html/material3/MdFab.kt
+++ compose-html-material3/src/jsMain/kotlin/com/huanshankeji/compose/html/material3/MdFab.kt
@@ -1,13 +1,7 @@
 package com.huanshankeji.compose.html.material3
 
-import androidx.compose.runtime.Composable
-import com.huanshankeji.compose.web.attributes.Attrs
-import com.huanshankeji.compose.web.attributes.attr
-import com.huanshankeji.compose.web.attributes.ext.label
-import com.huanshankeji.compose.web.attributes.slot
-import org.jetbrains.compose.web.attributes.AttrsScope
 import org.jetbrains.compose.web.dom.ElementScope
-import org.jetbrains.compose.web.dom.TagElement
+import org.w3c.dom.Element
 import org.w3c.dom.HTMLElement
 
 /*
@@ -16,57 +10,17 @@ https://material-web.dev/components/fab/
 https://material-web.dev/components/fab/stories/
  */
 
-@Composable
-private fun CommonMdFab(
-    tagName: String,
-    variant: String?,
-    size: String?,
-    label: String?,
-    lowered: Boolean?,
-    attrs: Attrs<HTMLElement>?,
-    content: @Composable (MdFabScope.() -> Unit)?
-) =
-    TagElement(tagName, {
-        variant?.let { attr("variant", it) }
-        size?.let { attr("size", it) }
-        label?.let { label(it) }
-        lowered?.let { attr("lowered", it) }
+fun <TElement : Element> SimplifiedTagElement(
+    content: (ElementScope<TElement>.() -> Unit)?
+) {
+    TODO()
+}
 
-        attrs?.invoke(this)
-    }, content?.let {
+class MdFabScope(val elementScope: ElementScope<HTMLElement>)
+
+private fun SimplifiedMdFab(
+    content: (MdFabScope.() -> Unit)?
+) =
+    SimplifiedTagElement(content?.let {
         { MdFabScope(this).it() }
     })
-
-@Composable
-fun MdFab(
-    variant: String? = null,
-    size: String? = null,
-    label: String? = null,
-    lowered: Boolean? = null,
-    attrs: Attrs<HTMLElement>? = null,
-    content: @Composable (MdFabScope.() -> Unit)?
-) {
-    require("@material/web/fab/fab.js")
-
-    CommonMdFab("md-fab", variant, size, label, lowered, attrs, content)
-}
-
-@Composable
-fun MdBrandedFab(
-    variant: String? = null,
-    size: String? = null,
-    label: String? = null,
-    lowered: Boolean? = null,
-    attrs: Attrs<HTMLElement>? = null,
-    content: @Composable (MdFabScope.() -> Unit)?
-) {
-    require("@material/web/fab/branded-fab.js")
-
-    CommonMdFab("md-branded-fab", variant, size, label, lowered, attrs, content)
-}
-
-
-class MdFabScope(val elementScope: ElementScope<HTMLElement>) {
-    fun AttrsScope<*>.slotEqIcon() =
-        slot("icon")
-}
