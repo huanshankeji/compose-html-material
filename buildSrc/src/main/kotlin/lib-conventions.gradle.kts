a505cda81113e33c354bb74a6ea45b1708a05cd3
Yongshun Ye <ShreckYe@gmail.com>
2026-02-15 18:54:21 +0800

    Set the ES2015 compilation target following https://kotlinlang.org/docs/whatsnew20.html#new-compilation-target

--- buildSrc/src/main/kotlin/lib-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/lib-conventions.gradle.kts
@@ -3,3 +3,11 @@ plugins {
     id("com.huanshankeji.kotlin-multiplatform-sonatype-ossrh-publish-conventions")
     id("com.huanshankeji.team.dokka.github-dokka-convention")
 }
+
+kotlin {
+    js {
+        compilerOptions {
+            target.set("es2015")
+        }
+    }
+}
