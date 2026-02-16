24c46d81757cf678b5c852c8e93a11f4f87d280c
Yongshun Ye <ShreckYe@gmail.com>
2026-02-16 22:32:01 +0800

    Comment out the ES2015 Gradle `kotlin` `js` configurations which are only needed in the final app project

--- buildSrc/src/main/kotlin/lib-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/lib-conventions.gradle.kts
@@ -4,10 +4,15 @@ plugins {
     id("com.huanshankeji.team.dokka.github-dokka-convention")
 }
 
+// To reduce the bundle size, this is only needed in the app project.
+// They might also be needed if the project is published to npm, but it's not now.
+/*
 kotlin {
     js {
         compilerOptions {
             target.set("es2015")
         }
+        useEsModules()
     }
 }
+*/
