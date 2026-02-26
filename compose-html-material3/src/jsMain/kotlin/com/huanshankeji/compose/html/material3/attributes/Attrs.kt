04b662d402b950f50d7f61b06a8fff77658f078b
Yongshun Ye <ShreckYe@gmail.com>
2026-02-27 05:08:11 +0800

    Golf

--- compose-html-material3/src/jsMain/kotlin/com/huanshankeji/compose/html/material3/attributes/Attrs.kt
+++ compose-html-material3/src/jsMain/kotlin/com/huanshankeji/compose/html/material3/attributes/Attrs.kt
@@ -3,6 +3,5 @@ package com.huanshankeji.compose.html.material3.attributes
 import com.huanshankeji.compose.web.attributes.attrIfNotNull
 import org.jetbrains.compose.web.attributes.AttrsScope
 
-fun AttrsScope<*>.indeterminate(indeterminate: Boolean?) {
+fun AttrsScope<*>.indeterminate(indeterminate: Boolean?) =
     attrIfNotNull("indeterminate", indeterminate)
-}
